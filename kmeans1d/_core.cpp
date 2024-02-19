0665aba5b4eb917af9ddb787c84ffd6679df3a68
Stephen Panaro <stephen.panaro@gmail.com>
2024-02-19 12:28:07 -0500

    Add Apple's support for weights
    In coremltools, Apple added support for weighted clustering. Add those
    changes verbatim.

--- kmeans1d/_core.cpp
+++ kmeans1d/_core.cpp
@@ -1,3 +1,27 @@
+// MIT License
+//
+// Copyright (c) 2019 Daniel Steinberg
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// Copyright © 2023 Apple Inc.
+
 #include <Python.h>
 
 #include <algorithm>
@@ -104,7 +128,7 @@ class CostCalculator {
     vector<double> cumsum2;
 
   public:
-    CostCalculator(const vector<double>& vec, ulong n) {
+    CostCalculator(const vector<double>& vec, ulong n, const vector<ulong>& /*sort_idxs*/) {
         cumsum.push_back(0.0);
         cumsum2.push_back(0.0);
         for (ulong i = 0; i < n; ++i) {
@@ -114,11 +138,60 @@ class CostCalculator {
         }
     }
 
+    double weight(ulong i, ulong j) {
+        return (i <= j) ? 1 + j - i : 0;
+    }
+
     double calc(ulong i, ulong j) {
         if (j < i) return 0.0;
         double mu = (cumsum[j + 1] - cumsum[i]) / (j - i + 1);
         double result = cumsum2[j + 1] - cumsum2[i];
         result += (j - i + 1) * (mu * mu);
+        result -= (2 * mu) * (cumsum[j + 1] - cumsum[i]);
+        return result;
+    }
+};
+
+/*
+ *  Weighted version of the CostCalculator
+ */
+class WeightedCostCalculator {
+    vector<double> cumw;
+    vector<double> cumsum;
+    vector<double> cumsum2;
+
+  public:
+    WeightedCostCalculator(
+            const vector<double>& vec,
+            ulong n,
+            const vector<ulong>& sort_idxs,
+            const double* unsorted_weights) {
+        vector<double> sorted_weights(n);
+        for (ulong i = 0; i < n; ++i) {
+            sorted_weights[i] = unsorted_weights[sort_idxs[i]];
+        }
+        cumw.push_back(0.0);
+        cumsum.push_back(0.0);
+        cumsum2.push_back(0.0);
+        for (ulong i = 0; i < n; ++i) {
+            double x = vec[i];
+            double w = sorted_weights[i];
+            cumw.push_back(w + cumw[i]);
+            cumsum.push_back(w * x + cumsum[i]);
+            cumsum2.push_back(w * x * x + cumsum2[i]);
+        }
+    }
+
+    double weight(ulong i, ulong j) {
+        return (i <= j) ? cumw[j + 1] - cumw[i] : 0.0;
+    }
+
+    double calc(ulong i, ulong j) {
+        if (j < i) return 0.0;
+        double w = weight(i, j);
+        double mu = (cumsum[j + 1] - cumsum[i]) / w;
+        double result = cumsum2[j + 1] - cumsum2[i];
+        result += w * (mu * mu);
         result -= (2 * mu) * (cumsum[j + 1] - cumsum[i]);
         return result;
     }
@@ -146,23 +219,14 @@ class Matrix {
     }
 };
 
-extern "C" {
-// "__declspec(dllexport)" causes the function to be exported when compiling on Windows.
-// Otherwise, the function is not exported and the code raises
-//   "AttributeError: function 'cluster' not found".
-// Exporting is a Windows platform requirement, not just a Visual Studio requirement
-// (https://stackoverflow.com/a/22288874/1509433). The _WIN32 macro covers the Visual
-// Studio compiler (MSVC) and MinGW. The __CYGWIN__ macro covers gcc and clang under
-// Cygwin.
-#if defined(_WIN32) || defined(__CYGWIN__)
-__declspec(dllexport)
-#endif
-void cluster(
-        double* array,
+template <typename CostCalculatorType, typename... CostArgsTypes>
+void cluster_impl(
+        const double* array,
         ulong n,
         ulong k,
         ulong* clusters,
-        double* centroids) {
+        double* centroids,
+        CostArgsTypes... args) {
     // ***************************************************
     // * Sort input array and save info for de-sorting
     // ***************************************************
@@ -186,7 +250,7 @@ void cluster(
 
     // Algorithm as presented in section 2.2 of (Gronlund et al., 2017).
 
-    CostCalculator cost_calculator(sorted_array, n);
+    CostCalculatorType cost_calculator(sorted_array, n, sort_idxs, args...);
     Matrix<double> D(k, n);
     Matrix<ulong> T(k, n);
 
@@ -233,7 +297,12 @@ void cluster(
         double centroid = 0.0;
         for (ulong i = t; i < t_; ++i) {
             sorted_clusters[i] = k_;
-            centroid += (sorted_array[i] - centroid) / (i - t + 1);
+            // Mean computation: this is only for squared L2 cost calculators
+            centroid += (
+                (sorted_array[i] - centroid)
+                * cost_calculator.weight(i, i)
+                / cost_calculator.weight(t, i)
+            );
         }
         centroids[k_] = centroid;
         k_ -= 1;
@@ -248,6 +317,41 @@ void cluster(
     for (ulong i = 0; i < n; ++i) {
         clusters[i] = sorted_clusters[undo_sort_lookup[i]];
     }
+}
+
+extern "C" {
+// "__declspec(dllexport)" causes the function to be exported when compiling on Windows.
+// Otherwise, the function is not exported and the code raises
+//   "AttributeError: function 'cluster' not found".
+// Exporting is a Windows platform requirement, not just a Visual Studio requirement
+// (https://stackoverflow.com/a/22288874/1509433). The _WIN32 macro covers the Visual
+// Studio compiler (MSVC) and MinGW. The __CYGWIN__ macro covers gcc and clang under
+// Cygwin.
+#if defined(_WIN32) || defined(__CYGWIN__)
+__declspec(dllexport)
+#endif
+void cluster(
+        double* array,
+        ulong n,
+        ulong k,
+        ulong* clusters,
+        double* centroids) {
+    cluster_impl<CostCalculator>(array, n, k, clusters, centroids);
+}
+
+#if defined(_WIN32) || defined(__CYGWIN__)
+__declspec(dllexport)
+#endif
+void cluster_with_weights(
+        double* array,
+        double* weights,
+        ulong n,
+        ulong k,
+        ulong* clusters,
+        double* centroids) {
+    cluster_impl<WeightedCostCalculator, const double*>(
+        array, n, k, clusters, centroids, weights
+    );
 }
 } // extern "C"
 
